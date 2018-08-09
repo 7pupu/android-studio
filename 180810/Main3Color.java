508c4803faf2a06998180e2c08d95779579e8163
alice <wotupset@gmail.com>
2018-08-10 00:06:45 +0800

    Add files via upload

---
+++ 180810/Main3Color.java
@@ -0,0 +1,79 @@
+package com.example.ppp180312.myapplication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Main3Color extends AppCompatActivity {
+    Button btn;
+    String result;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3_color);
+
+        poi();
+        result="";
+        try{
+            result+=poi2();
+        }
+        catch (Exception e){
+            result+="\r\n"+"錯誤";
+            result+="\r\n"+e.getMessage();
+            result+="\r\n"+e.getClass();
+        }
+        AlertDialog.Builder builder=new AlertDialog.Builder(Main3Color.this);
+        builder.setMessage(""+result).show();
+
+
+    }
+
+    private String poi2() {
+        result="";
+        return result;
+    }
+
+    private void poi() {
+/*
+Color.BLACK
+Color.BLUE
+Color.CYAN
+Color.DKGRAY
+Color.GRAY
+Color.GREEN
+Color.LTGRAY
+Color.MAGENTA
+Color.RED
+Color.TRANSPARENT
+Color.WHITE
+Color.YELLOW
+* */
+        tv = this.findViewById(R.id.textView4);
+        result="";
+        //result=""+getApplicationContext().getApplicationInfo();
+
+        //tv.setTextColor(android.graphics.Color.RED);
+        tv.setBackgroundColor(Color.rgb(255,255,0));
+        int pp=Color.parseColor("#FF0000");
+        //result=""+pp;
+        tv.setTextColor(pp);
+        tv.setTextSize(50);//14sp=default
+        result=""+findViewById(R.id.all);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Main3Color.this);
+        //builder.setTitle("對話視窗");
+        builder.setMessage(""+result);
+        //builder.show();
+
+
+    }
+}
