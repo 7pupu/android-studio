588ffb7418b0bc04b4c74d50ed6aedc76f23f3a8
alice <wotupset@gmail.com>
2018-08-15 18:41:27 +0800

    Add files via upload

--- 180815/MainActivity.java
+++ 180815/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.ppp180312.myapplication;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,22 +26,9 @@ public class MainActivity extends AppCompatActivity {
         try{if(1==1){}}
         catch (Exception e){}
         //
-        try{if(1==1){
+        try{if(1==0){
             result="";
-            result+="\r\n"+this.getFilesDir();
-            String fname=new File(getFilesDir(), "ppp.png").getAbsolutePath();
-            result+="\r\n"+fname;
-
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inJustDecodeBounds = true;
-
-            Bitmap bMap = BitmapFactory.decodeFile(fname,options);
-            result+="\r\n"+options.outWidth +"x"+options.outHeight;
-
-            //this.getResources();
-
-            //Toast toast = Toast.makeText(MainActivity.this,""+result, Toast.LENGTH_LONG);
-            //toast.show();
+            result+="\r\n"+this.getResources();
             tv = (TextView)findViewById(R.id.aaa);
             tv.setText(""+result);
 
@@ -51,6 +40,74 @@ public class MainActivity extends AppCompatActivity {
 
             Toast toast = Toast.makeText(MainActivity.this,""+result, Toast.LENGTH_LONG);
             toast.show();
+
+        }
+
+        //
+        try{if(1==1){
+            result="";
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+
+            //從檔案路徑
+            //String fn=new File(getFilesDir(), "ppp.png").getAbsolutePath();
+            //InputStream in = getContentResolver().openInputStream( Uri.parse(fn));
+            //BitmapFactory.decodeStream(in, null, options);
+
+            //BitmapFactory.decodeFile(fn,options);
+
+            //從資源路徑
+            //Bitmap bmp=BitmapFactory.decodeResource(getResources(), R.drawable.ppp, options);
+            options.inJustDecodeBounds = false;
+            Bitmap bmp=BitmapFactory.decodeResource(getResources(), R.drawable.ppp, options);
+
+            result+="\r\n"+bmp;
+            result+="\r\n"+options.outWidth +"x"+options.outHeight;
+            result+="\r\n";
+
+
+            String html="<img src='"+R.drawable.ppp+"' width='100' height='100'/>";
+            //Spanned sp
+            Html.ImageGetter ig =new Html.ImageGetter() {
+                @Override
+                public Drawable getDrawable(String source) {
+                    int id = Integer.parseInt(source);
+                    Drawable d = getResources().getDrawable(id);
+                    //d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+                    d.setBounds(0, 0, d.getIntrinsicWidth() /2, d.getIntrinsicHeight() /2);
+                    //d.setBounds(0, 0, 100, 100);
+
+                    return d;
+                }
+            };
+            //CharSequence charSequence = Html.fromHtml(html,ig, null);
+
+
+
+
+
+            //this.getResources();
+
+            //Toast toast = Toast.makeText(MainActivity.this,""+result, Toast.LENGTH_LONG);
+            //toast.show();
+
+
+            tv = (TextView)findViewById(R.id.aaa);
+            tv.setText(""+result);
+            tv.append(Html.fromHtml(html,ig, null));
+
+        }}
+        catch (Exception e){
+            result="";
+            result+="\r\n"+e.getClass();
+            result+="\r\n"+e.getMessage();
+
+            Toast toast = Toast.makeText(MainActivity.this,""+result, Toast.LENGTH_LONG);
+            toast.show();
+
+            tv = (TextView)findViewById(R.id.aaa);
+            tv.setText(""+result);
 
         }
 
